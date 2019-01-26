f6c5513f6b5015ce28f3a66104c5b700fddaf718
alexxsanya <alexxsanya@gmail.com>
2019-01-26 23:36:38 +0300

    [#163499138] Removed unneccessary code from this file

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-
-    return("Welcome to Kampala Accidents Response Unit System")
-
-
+from views.routes import app
 
 if __name__ == "__main__":
     app.run(debug=True)
