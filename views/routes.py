37eb2a2633e4bf16e3c314e101ea1dc629a494f0
alexxsanya <alexxsanya@gmail.com>
2019-01-28 15:57:50 +0300

    [#163513549] Added error handlers for 401,403, 404 and 500

--- views/routes.py
+++ views/routes.py
@@ -10,3 +10,18 @@ def home():
     return render_template("home.html")
 
 
+@app.errorhandler(401)
+def error_401(error):
+    return render_template("401.html")
+
+@app.errorhandler(403)
+def error_401(error):
+    return render_template("403.html")
+
+@app.errorhandler(404)
+def error_401(error):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def error_401(error):
+    return render_template("500.html")
