df582f0bca55b3aac765ee018cfa54c620f4a921
alexxsanya <alexxsanya@gmail.com>
2019-02-06 15:26:37 +0300

    [#163743352] Improved the config structure

--- config.py
+++ config.py
@@ -1,13 +1,23 @@
+from os import environ
+
 class Config(object):
     DEBUG = False
     TESTING = False
-    DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "secret_key"
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    DEBUG = False
+    ENV = "production"
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
 
 class DevelopmentConfig(Config):
+    ENV = "development"
+    DEVELOPMENT = True
     DEBUG = True
 
 class TestingConfig(Config):
