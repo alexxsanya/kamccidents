5e4fa69c847b03cbb87237d854dfd3faed0baebb
alexxsanya <alexxsanya@gmail.com>
2019-02-08 16:48:10 +0300

    [#163526750] Commit to User Model 	- Add the phone column to the model 	- Add a method to return a user for the supllied email address

--- models/user_model.py
+++ models/user_model.py
@@ -9,18 +9,21 @@ class UsersModel(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key=True)
-  firstname = db.Column(db.String(128), nullable=False)
-  lastname = db.Column(db.Text, nullable=False)
+  firstname = db.Column(db.String(25), nullable=False)
+  lastname = db.Column(db.String(25), nullable=False)
   email = db.Column(db.Text,nullable=False)
   password = db.Column(db.Text, nullable=False)
+  phone = db.Column(db.String(12),nullable=False)
   created_at = db.Column(db.DateTime)
   modified_at = db.Column(db.DateTime)
 
   def __init__(self, data):
+    print(">>> {} ".format(data))
     self.firstname = data.get('firstname')
     self.lastname = data.get('lastname')
-    self.lastname = data.get('email') 
+    self.email = data.get('email') 
     self.password = self.__generate_hash(data.get('password'))
+    self.phone = data.get('phone')
     self.created_at = datetime.datetime.utcnow()
     self.modified_at = datetime.datetime.utcnow()
 
@@ -56,7 +59,9 @@ class UsersModel(db.Model):
   
   def check_hash(self, password):
     return bcrypt.check_password_hash(self.password, password)
-
+  @staticmethod
+  def get_user_by_email(email):
+    return UsersModel.query.filter_by(email=email).first()
 # add this class
 class UserSchema(Schema):
   """
@@ -67,5 +72,6 @@ class UserSchema(Schema):
   lastname = fields.Str(required=True)
   email = fields.Email(required=True)
   password = fields.Str(required=True)
+  phone = fields.Str(required=True)
   created_at = fields.DateTime(dump_only=True)
   modified_at = fields.DateTime(dump_only=True) 
--- views/routes.py
+++ views/routes.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, jsonify, flash, redirect, url_for, request
+from flask import Flask, render_template, jsonify, flash, redirect, url_for, request, Response
 import json
 import os
 from . import init_app
 
 app = init_app()
+
+from models.user_model import UserSchema,UsersModel
 
 @app.route("/")
 def home():
@@ -35,8 +37,48 @@ def personal_report():
 
 @app.route("/login",methods=['POST'])
 def login_user():
+    user = request.form.to_dict(flat=True)
+    #print(user.get("user_pass"))
+    user = UsersModel.get_user_by_email(user.get('user_name'))
+    print(user)
     flash('You were successfully logged in')
     return redirect(url_for('dashboard'))
+
+@app.route("/create-user",methods=['POST'])
+def create_user():
+    req_data = request.form.to_dict(flat=True)
+
+    req_data['firstname'],req_data['lastname'] =req_data.get('name').split(",") 
+
+    print(">>> {} ".format(req_data))
+    
+    data, error = UserSchema().load(req_data)
+
+    if error:
+        return custom_response(error, 400)
+  
+  # check if user already exist in the db
+    user_in_db = UsersModel.get_user_by_email(data.get('email'))
+    if user_in_db:
+        message = {'error': 'User already exist, please supply another email address'}  
+        return custom_response(message, 400)
+
+    user = UsersModel(data)
+    user.save()
+
+    ser_data = UserSchema().dump(user).data
+
+    return custom_response({'data': ser_data}, 201)
+
+def custom_response(res, status_code):
+  """
+  Custom Response Function
+  """
+  return Response(
+    mimetype="application/json",
+    response=json.dumps(res),
+    status=status_code
+  )
 
 @app.errorhandler(401)
 def error_401(error):
