01d320daf008ea607fcf9d00f093a15f892d7756
alexxsanya <alexxsanya@gmail.com>
2019-02-12 18:50:21 +0300

    [#163503424] Added a function to pick current location

--- views/static/js/util.js
+++ views/static/js/util.js
@@ -16,3 +16,27 @@ function close_display(){
     $(".photo-display").css("display","none");
     console.log("clicked")
 }
+
+function pickCoordinates() {
+    var output = $("#accident-address");
+  
+    if (!navigator.geolocation){
+      output.value = "Geolocation is not supported by your browser";
+      return;
+    }
+  
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      output.value = latitude + ',' + longitude;
+    }
+  
+    function error() {
+      output.value = "Unable to retrieve your location";
+    }
+  
+    output.value = "Decoding…";
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+    
+  }
