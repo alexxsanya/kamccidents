e404c656625c29e47e9cc4bb9f5e74b55d8b5687
alexxsanya <alexxsanya@gmail.com>
2019-02-24 00:17:38 +0300

    JS file with functionality to generate routes on google maps

---
+++ views/static/js/route.js
@@ -0,0 +1,127 @@
+function generateRoute() {
+
+    var kampalaCity = new google.maps.LatLng(0.316644, 32.589536);
+    
+    var map = new google.maps.Map(document.getElementById('route-map'), {
+      mapTypeControl: false,
+      center: kampalaCity,
+      zoom: 13
+    });
+  
+    new AutocompleteDirectionsHandler(map);
+  }
+  
+  /**
+   * @constructor
+   */
+  function AutocompleteDirectionsHandler(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(map);
+  
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-input');
+    var modeSelector = document.getElementById('mode-selector');
+    
+    var originAutocomplete = 
+        new google.maps.places.Autocomplete(originInput);
+    // Specify just the place data fields that you need.
+    originAutocomplete.setFields(['place_id']);
+  
+    var destinationAutocomplete =
+        new google.maps.places.Autocomplete(destinationInput);
+    // Specify just the place data fields that you need.
+    destinationAutocomplete.setFields(['place_id']);
+  
+    this.setupClickListener('changemode-walking', 'WALKING');
+    this.setupClickListener('changemode-transit', 'TRANSIT');
+    this.setupClickListener('changemode-driving', 'DRIVING');
+  
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+  
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+        destinationInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+  
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  AutocompleteDirectionsHandler.prototype.setupClickListener = function(
+      id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+  
+    radioButton.addEventListener('click', function() {
+      me.travelMode = mode;
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
+      autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+  
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+  
+      if (!place.place_id) {
+        window.alert('Please select an option from the dropdown list.');
+        return;
+      }
+      if (mode === 'ORIG') {
+        me.originPlaceId = place.place_id;
+      } else {
+        me.destinationPlaceId = place.place_id;
+      }
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+    var me = this;
+  
+    this.directionsService.route(
+        {
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        },
+        function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+  };
+
+  function geocodeLatLng() {
+    
+    var geocoder = new google.maps.Geocoder;
+    //var input = document.getElementById('latlng').value;
+    var input = '0.314069, 32.587187'
+    var latlngStr = input.split(',', 2);
+    var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+    geocoder.geocode({'location': latlng}, function(results, status) {
+      if (status === 'OK') {
+        if (results[0]) {
+          console.log(results[0].formatted_address)
+          return results[0].formatted_address
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
