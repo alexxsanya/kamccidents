17b115ce96b9681535511ecaf0bd3ff5b6ced43a
alexxsanya <alexxsanya@gmail.com>
2019-03-26 14:35:35 +0300

    heatmap code refactor

--- views/static/js/map.js
+++ views/static/js/map.js
@@ -15,25 +15,17 @@ function getPosition(position) {
 function loadHeatMap(){
   document.querySelector('beacon').setAttribute('style',"display:inline");
   document.getElementById('hospital-map-label').setAttribute('hidden','hidden');
-  /* Data points defined as an array of LatLng objects */
-/* Data points defined as a mixture of WeightedLocation and LatLng objects */
-  var heatMapData = [
-    {location: new google.maps.LatLng(0.318522, 32.595459), weight: 0.5},
-    new google.maps.LatLng(0.317127, 32.593764),
-    {location: new google.maps.LatLng(0.318726, 32.591650), weight: 15},
-    {location: new google.maps.LatLng(0.314885, 32.594622), weight: 40},
-    {location: new google.maps.LatLng(0.316762, 32.588399), weight: 2},
-    new google.maps.LatLng(0.313898, 32.586843),
-    {location: new google.maps.LatLng(0.314069, 32.587187), weight: 17},
-    {location: new google.maps.LatLng(0.313501, 32.592562), weight: 3},
-    {location: new google.maps.LatLng(0.312224, 32.587670), weight: 7},
-    new google.maps.LatLng(0.316805, 32.585309),
-    {location: new google.maps.LatLng(0.318704, 32.585889), weight: 0.5},
-    new google.maps.LatLng(0.313887, 32.586886),
-    {location: new google.maps.LatLng(0.318543, 32.584719), weight: 21},
-    {location: new google.maps.LatLng(0.319251, 32.587948), weight: 6}
-  ];
 
+  headmap_db = []
+  $.getJSON('/all-accidents', function(json1) {
+      $.each(json1, function(key, data) {
+        var geo = data.acc_location.split(',') 
+        console.log(data)
+        headmap_db.push({location: new google.maps.LatLng(geo[0], geo[1]), weight: 0.5})
+      });
+  });
+
+  console.log(headmap_db)
   var style = [
     {
       "featureType": "administrative",
@@ -118,16 +110,17 @@ function loadHeatMap(){
 
   map = new google.maps.Map(document.getElementById('mapCanvas'), {
     center: kampalaCity,
-    zoom: 15,
+    zoom: 14,
     mapTypeId: 'roadmap',
     styles: style
   });
 
   var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatMapData
+    data: headmap_db
   });
 
   heatmap.setMap(map);
+
 }
 
 
